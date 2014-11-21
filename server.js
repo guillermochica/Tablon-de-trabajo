78ff172fded631e97314ae50b505cbccf90ba13b
guillermochica <1bgchica@gmail.com>
2014-11-21 13:56:48 +0100

    Ahora crea tablas estáticas

--- server.js
+++ server.js
@@ -12,17 +12,22 @@ app.get('/', function (req, res) {
   res.send(inicio);
 });
 
-app.get('/tablon/:jefe/:project', function (req, res) {
+app.get('/tablon/:jefe/:project/:datos', function (req, res) {
   res.send(tablas[req.params.jefe][req.params.project]);
 
 });
 
-app.post('/tablon/:jefe/:project', function (req, res) {
+app.post('/tablon/:jefe/:project/:datos', function (req, res) {
   //Creación de la matriz de proyectos:
   tablas[req.params.jefe] = new Array; //Crea un array para un jefe concreto; cada jefe puede llevar más de un proyecto
 
   var page = fs.createWriteStream('tablon' + req.params.jefe + req.params.project + '.html'); //Crea el documento html donde se ubicará el tablón
-  page.write('<h1>Tablón de ' + req.params.project + ' / Jefe: ' + req.params.jefe + '</h1><br>'); //Escribe el tablón
+
+  var datos = new Array;
+
+  toInt(datos, req.params.datos); //los datos vienen en string, hay que pasarlos a entero
+
+  crearTablon(page,req.params.project,req.params.jefe, datos);  //Escribe el tablón
 
   tablas[req.params.jefe][req.params.project] = fs.readFileSync('tablon' + req.params.jefe + req.params.project + '.html', 'utf8'); //Carga el tablón en la matriz de proyectos
   res.send('Creado tablón para proyecto ' + req.params.project);
@@ -31,3 +36,56 @@ app.post('/tablon/:jefe/:project', function (req, res) {
 
 app.listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
+
+//Funciones
+
+function crearTablon (pagina, project, jefe, datos) {
+  var string = '<html> \
+    <head> \
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> \
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> \
+      <title>Tablón de trabajo</title> \
+    </head> \
+    <body align="center"> \
+    <h1>Tablón de ' + project + ' / Jefe: ' + jefe + '</h1><br> <p></p> \
+    <table border="1" style="width:100%"> \
+    <tr>'
+
+  for (i = 1 ; i<=datos[0] ; i++) {
+    string = string + '<td> Paco </td>';
+  }
+  string = string + '</tr>';
+
+  for (i = 1 ; i<=datos[1] ; i++) {
+    string = string + '<tr>';
+    for (j = 1 ; j<=datos[0] ; j++) {
+      string = string + '<td> Tarea </td>';
+    }
+    string = string + '</tr>';
+  }
+  string = string + '<p>No pierdas el tablón, guarda esta página en marcadores</p></body></html>';
+
+
+  pagina.write(string);
+}
+
+function toInt(datos, params) {
+  var k = 0;
+
+  var aux1 = '';
+  var i = 0;
+  while (params[i] != ',') {
+    aux1 = aux1 + params[i];
+    i++;
+  }
+  i++;
+  datos[k] = parseInt(aux1, 10); //primer numero guardado
+  k++;
+  var aux2 = '';
+  while (i != params.length) {
+    aux2 = aux2 + params[i];
+    i++;
+  }
+  datos[k] = parseInt(aux2, 10); //segundo numero guardado
+  k++;
+}
